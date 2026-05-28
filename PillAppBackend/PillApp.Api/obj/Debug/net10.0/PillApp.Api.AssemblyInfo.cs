f77ff1dc8eab4a1a97d811f33960007448acb0b0
F-Dimita89 <f.dimita1989@gmail.com>
2026-05-28 14:00:05 +0200

    docker2

--- PillAppBackend/PillApp.Api/obj/Debug/net10.0/PillApp.Api.AssemblyInfo.cs
+++ PillAppBackend/PillApp.Api/obj/Debug/net10.0/PillApp.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PillApp.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e52758cc86a2f3d1bad423b0c2377027a12c39e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b2d20ff3df68108cc70b0423faa99a321ec44cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("PillApp.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PillApp.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
